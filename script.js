6db24b3d8c350da5151c37dfcea3f245a60af013
SaiedIslam <saiedislam61@gmail.com>
2021-10-20 16:22:50 +0100

    change

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-while(1) {
+ 
     let numIn = parseInt(prompt("Enter Number"))
      
     let result = numIn % 2
-     
-    console.log(Boolean(!(result == 1)))
-    console.log(Boolean(result == 0))
-    }
     
-    
-    while(1){
     let num1 = parseInt(prompt("Enter Number"))
     let num2 = parseInt(prompt("Enter Number"))
     let result = num1 % num2
      
-    console.log(Boolean(result == 0))
-    }
     
     
     let numOfStudents = 344
