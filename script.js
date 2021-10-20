97349504ca1cb44fc12030578ef45bcede8b0d48
SaiedIslam <saiedislam61@gmail.com>
2021-10-20 15:47:57 +0100

    added stuff

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 while(1) {
-    let numIn = parseInt(prompt("4"))
+    let num1 = parseInt(prompt("9"))
+    let num2= parseInt(prompt("6"))
 
     let result = numIn % 2
 
     console.log(Boolean(!(result == 1)))
     console.log(Boolean(result = 0))
 }
-while(1){
-    let num1 = parseInt(prompt("5"))
-    let num1 = parseInt(prompt("9"))
-}
+
+
 
