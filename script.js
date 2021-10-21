e02e85a90f79b16b33a64552ec41538d7d530f27
SaiedIslam <saiedislam61@gmail.com>
2021-10-21 14:16:45 +0100

    random

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
     
 
-let Days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-alert("Pick a number");
+/* let Days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let DOW = (prompt("Pick a number"))
 let dayNumber = Math.floor(Math.random(0,6)*10)
 
 if (dayNumber === 0) {
-  console.log(Days[0]);
+  console.log(Days[0])
 }
 else if (dayNumber === 1) {
-    console.log(Days[1]);
+    console.log(Days[1])
 }
 else if (dayNumber === 2) {
-    console.log(Days[2]);
+    console.log(Days[2])
 }
 else if (dayNumber === 3) {
-    console.log(Days[3]);
+    console.log(Days[3])
 }
 else if (dayNumber === 4) {
-    console.log(Days[4]);
+    console.log(Days[4])
 }
 else if (dayNumber === 5) {
-    console.log(Days[5]);
+    console.log(Days[5])
 }
 else if (dayNumber === 6) {
-    console.log(Days[6]);
+    console.log(Days[6])
 }
 else if (dayNumber > 6) 
 {
   alert("Incorrect day number")
-} 
+} */
 
+let Days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let n = prompt("Enter Day Number")
+
+let result= "Invalid Day number"
+if (isNUmber(n)   ) {
+        if (n>=0 && n<=6) {
+                result=Days[n]
+        }
+} 
+alert (result)
+
+function isNUmber(v){
+    return!isNaN(v)
+}
